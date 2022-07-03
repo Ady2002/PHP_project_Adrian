7924e664686f66e2de335ff99543283ab26dc8af
Ady2002 <ady20002002@gmail.com>
2022-07-03 20:09:23 +0300

    comisia numarul 4

--- src/Controller/MainChargeAppController.php
+++ src/Controller/MainChargeAppController.php
@@ -4,16 +4,16 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainChargeAppController extends AbstractController
 {
-    #[Route('/main/charge/app', name: 'app_main_charge_app')]
-    public function index(): JsonResponse
+    #[Route('/')]
+    public function index(): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/MainChargeAppController.php',
-        ]);
+        return new Response(
+
+        );
     }
 }
